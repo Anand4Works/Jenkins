261cef8ad5d6c13a53e1f31360b057c9d8f46e13
anand <anand@Anand-Seena>
2023-01-09 16:15:45 +0530

    first commit

---
+++ JenkinsPro/src/main/java/Demo/Test2.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void tc2() 
+	{
+		Reporter.log("Hii world",true);
+	}
+}
