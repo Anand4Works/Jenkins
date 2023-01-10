5cb6700325ff06f8b8cbf0d9e1faa1ac2b40bd25
anand <anand@Anand-Seena>
2023-01-10 15:45:18 +0530

    Third commit

---
+++ JenkinsPro/src/main/java/Demo/Tes3.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Tes3 {
+	@Test
+	public void m3()
+	{
+		System.out.println("Hello World");
+	}
+
+}
